e28bbd8bd42d697c2832073705b74f0ebb1b8231
Ubuntu <ubuntu@ip-172-31-18-185.ec2.internal>
2019-06-22 02:48:29 +0000

    examplar launcher

--- build/launch.sh
+++ build/launch.sh
@@ -1,1 +1,3 @@
-mpiexec -np 64 -x LD_LIBRARY_PATH --hostfile hosts all_reduce_perf -b 576M -e 576M -g 1
+count=`cat hosts | wc -l`
+ele=576M
+mpiexec -np $count -x LD_LIBRARY_PATH --hostfile hosts all_reduce_perf -b $ele -e $ele -g 1 -n 10 -w 0
